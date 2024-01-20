5720c1528579ebad434d54bccaa301f368144a48
pranavsukali <155296830+pranavsukali@users.noreply.github.com>
2024-01-20 19:40:46 +0000

    summary

--- app.py
+++ app.py
@@ -11,25 +11,28 @@ def index():
 
 @app.route("/summarize", methods=["GET", "POST"])
 def summarize():
-  if rq.method == "POST":
-    API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-    headers = {"Authorization": "Bearer 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"}
-    url=rq.form["url_search"]
-    id=get_youtube_video_id(url)
-    data=get_transcript(id)
-    minL=80
-    maxL=150
-    def query(payload):
-      response = requests.post(API_URL, headers=headers, json=payload)
-      return response.json()
-    
-    output = query({
-      "inputs": data,
-      "parameters":{"min_length":minL, "max_length":maxL}
-    })
-    return render_template("index.html", results=output)
-  else:
-    return render_template("index.html")
+    if rq.method == "POST":
+        # Your existing code for handling POST requests goes here
+        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+        headers = {"Authorization": "Bearer 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"}
+        url=rq.form["url_search"]
+        id=get_youtube_video_id(url)
+        data=get_transcript(id)
+        minL=80
+        maxL=150
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+        
+        output = query({
+            "inputs": data,
+            "parameters":{"min_length":minL, "max_length":maxL}
+        })
+        return render_template("index.html", results=output)
+    else:
+        # Your existing code for handling GET requests goes here
+        return render_template("index.html")
+
 
 def get_youtube_video_id(url):
     # Parse the URL
